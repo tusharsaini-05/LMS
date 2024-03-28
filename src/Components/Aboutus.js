8aaf26ee6dce926ee40fb76c6a0e910eabc5c316
Sanket <sanketyelave11@gmail.com>
2024-03-28 11:50:57 +0530

    First commit

---
+++ src/Components/Aboutus.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Aboutus() {
+    return (
+        <div className='aboutus-bg h-[40rem]'>
+
+//Bahi about su for hero section ka code isi me likh de.
+
+
+        </div>
+    );
+}
+
+export default Aboutus;
